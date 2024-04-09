aab1921c0b336ac4b91a92425c0456a74beb514f
Serwes Bhatta <serwes@dzangolab.com>
2024-04-08 23:41:38 -0500

    feat: create a function to input elements

---
+++ heapSort.py
@@ -0,0 +1,6 @@
+def inputElements(array):
+  n = int(input("Enter the number of elements you want to sort : "))
+
+  for i in range(n):
+    element = int(input(f"Enter element {i+1}: "))
+    array.append(element)
